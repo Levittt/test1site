d92dbf0d1d882c6693fa3c8f1223c7eca4db4227
Levittt <aireekie@mail.ru>
2020-12-01 14:10:53 +0500

    upd 2020/12/01

---
+++ send.php
@@ -0,0 +1,30 @@
+<?php
+$fullname = $_POST['fullname'];
+$email = $_POST['email'];
+$msg = $_MSG['msg'];
+
+$fullname = htmlspecialchars($fullname);
+$email = htmlspecialchars($email);
+$msg = htmlspecialchars($msg);
+
+$fullname = urldecode($fullname);
+$email = urldecode($email);
+$msg = urldecode($msg);
+
+$fullname = trim($fullname);
+$email = trim($email);
+$msg = trim($msg);
+
+//echo $fullname;
+//echo "<br>";
+//echo $email;
+//echo "<br>";
+//echo $msg;
+
+if (mail("learn-python@mail.ru", "Комментарий с learnpython", "Имя:".$fullname.". E-mail: ".$email." Msg: ".$msg." ,"From: example2@mail.ru \r\n"))
+ {
+    echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}
+}?>
