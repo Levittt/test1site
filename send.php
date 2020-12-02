cfda202a5097dfd1ab20e131bca6a1f649d54de5
Levittt <aireekie@mail.ru>
2020-12-02 15:33:55 +0500

    upd 2020/12/02

--- send.php
+++ send.php
@@ -4,7 +4,7 @@ require("/phpailer.php");
 require("/smtp.php");
 
 
-$mail = new PHPMailer;
+$mail = new PHPMailer\PHPMailer\PHPMailer();
 $mail->CharSet = 'utf-8';
 
 $name = $_POST['fullname'];
@@ -12,9 +12,10 @@ $email = $_POST['email'];
 //$pas = $_POST['password'];
 $msg = $_POST['msg'];
 
-$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->isSMTP();
+$mail->SMTPDebug = 2;
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+$mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};                               // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'levitisacat@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
