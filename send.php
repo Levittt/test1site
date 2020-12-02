34f78b6cb1eae0479be8a3141d30d69079332c04
Levittt <aireekie@mail.ru>
2020-12-02 15:15:48 +0500

    upd 2020/12/02

--- send.php
+++ send.php
@@ -1,7 +1,9 @@
 <?php 
 
-require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
+require("/home/site/libs/PHPMailer-master/src/PHPMailer.php");
+require("/home/site/libs/PHPMailer-master/src/SMTP.php");
+
+$mail = new PHPMailer\PHPMailer\PHPMailer();
 $mail->CharSet = 'utf-8';
 
 $name = $_POST['fullname'];
